379d03dee7ac05cecbe8376c8709a8b10b9d9e7c
Simon Cook <simon.cook@salesforce.com>
2016-10-12 13:18:59 +0100

    Added Response Card

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 var AlexaAppServer = require('alexa-app-server');
-AlexaAppServer.start({
-    server_root:__dirname,     		// Path to root
-    public_html:"public_html", 		// Static content
-    app_dir:"apps",            		// Where alexa-app modules are stored
-    app_root:"/alexa/",        		// Service root
+
+//create a new server with the appropriate attributes
+var server = new AlexaAppServer({
+    server_root:__dirname,     		      // Path to root
+    public_html:"public_html", 		      // Static content
+    app_dir:"apps",            		      // Where alexa-app modules are stored
+    app_root:"/alexa/",        		      // Service root
     port:process.env.PORT || 8080       // What port to use, duh
+});
+
+//start the server
+server.start();
+
+//enables CORS on express
+server.express.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
